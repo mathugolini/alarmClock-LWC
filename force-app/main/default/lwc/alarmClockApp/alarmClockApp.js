b4a3aac89fb98a6ee2f4f4eaeef780a38f0da18a
Matheus <mathugolini@gmail.com>
2023-07-19 13:36:26 -0300

    Regra de negocio para display de horas js

--- force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
+++ force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
@@ -2,4 +2,32 @@ import { LightningElement } from 'lwc';
 import AlarmClockAssets from '@salesforce/resourceUrl/AlarmClockAssets'
 export default class AlarmClockApp extends LightningElement {
     clockImage = AlarmClockAssets+'/AlarmClockAssets/clock.png'
+    currentTime = ''
+
+    connectedCallback() {
+        this.currentTimeHandler()
+    }
+
+    currentTimeHandler() {
+        setInterval(()=>{
+
+        let dateTime = new Date()
+        let hour = dateTime.getHours()
+        let min = dateTime.getMinutes()
+        let sec = dateTime.getSeconds()
+        let ampm = "AM"
+        if(hour == 0) {
+            hour == 12
+        } else if (hour>=12) {
+            hour = hour - 12
+            ampm = "PM"
+        }
+        hour = hour<10? "0"+hour:hour 
+        min = min<10? "0"+min:min
+        sec = sec<10? "0"+sec:sec
+
+        this.currentTime = `${hour}:${min}:${sec} ${ampm}`
+
+        }, 1000)
+    }
 }
